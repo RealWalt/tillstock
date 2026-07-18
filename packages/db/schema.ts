2cda546487128172875d5f2cbce5ad0d87da4e68
RealWalt <mchlsilvero@gmail.com>
2026-07-18 20:44:37 -0300

    feat: add Drizzle ORM with PostgreSQL and users schema

---
+++ packages/db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable('users', {
+    id: uuid().defaultRandom().primaryKey(),
+    name: text('name').notNull(),
+    email: text('email').notNull().unique(),
+    createdAt: timestamp().defaultNow().notNull(),
+})
